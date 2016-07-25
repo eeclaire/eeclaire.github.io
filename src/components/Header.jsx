aee7bd35895948b335eca754dd45997041e5fbdc
Claire Durand <claire.durand@temple.edu>
2016-07-25 01:26:55 -0400

    preliminary links to github and twitter

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,29 @@ import React from 'react';
 
 import { Link } from 'react-router';
 
+
+var githubIcon = './static/' + require('../img/github.svg');
+var twitterIcon = './static/' + require('../img/twitter.svg');
+
 const Header = React.createClass({
 	render() {
 		return (
-			<h1 id="title">
-				<Link to="/">
-					Claire Durand
-				</Link>
-			</h1>
+			<div>
+				<h1 id="title">
+					<Link to="/">
+						Claire Durand
+					</Link>
+					<span className="icons">
+						<a href="https://github.com/eeclaire">
+							<img src={githubIcon} />
+						</a>
+						<a href="https://twitter.com/notfrenchclaire">
+							<img src={twitterIcon} />
+						</a>
+					</span>
+				</h1>
+
+			</div>
 		);
 	}
 })
