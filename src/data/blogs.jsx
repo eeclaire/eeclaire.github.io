f9de9441096444a94972ed8834f9574271e24044
Claire Durand <claire.durand@temple.edu>
2016-08-20 23:17:08 -0400

    First iteration of personal photos

--- src/data/blogs.jsx
+++ src/data/blogs.jsx
@@ -2,7 +2,7 @@ const blogList = [
 	{
 		"title": "Hello World",
 		"date": "August 19th, 2016",
-		"content": "Just trying this out!",
+		"content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 		"id":1
 	}
 ];
--- src/data/photos.jsx
+++ src/data/photos.jsx
@@ -1,23 +1,13 @@
 const photoList = [
 	{
-		"url":"http://petattack.com/wp-content/uploads/2014/08/Beautifull-cat-cats-14749885-1600-1200.jpg",
-		"title":"cat",
+		"url":"src/img/photos/phl_chinatown.jpg",
+		"title":"Chinatown, PHL",
 		"id":1
 	},
 	{
-		"url":"https://pbs.twimg.com/profile_images/378800000532546226/dbe5f0727b69487016ffd67a6689e75a.jpeg",
-		"title":"bow-tie cat",
+		"url":"src/img/photos/roboboat.jpg",
+		"title":"Roboboat @ Core Creek Park",
 		"id":2
-	},
-	{
-		"url":"http://i2.cdn.turner.com/cnnnext/dam/assets/150324154010-04-internet-cats-restricted-super-169.jpg",
-		"title":"two face cat",
-		"id":3
-	},
-	{
-		"url":"http://www.ualr.edu/giellis/assets/images/catbread2.jpg",
-		"title":"toast cat",
-		"id":4
 	}
 ];
 
