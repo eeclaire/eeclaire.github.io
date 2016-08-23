dd4b3888c459f0b667ea4c40af9535099b758d62
Claire Durand <claire.durand@temple.edu>
2016-08-23 15:05:26 -0400

    added color-my-www project

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -1,10 +1,17 @@
 const projectList = [
 	{
+		"title": "Color my www",
+		"image": "src/img/projects/color-my-www_example.png",
+		"description": "Intelligent website color choices",
+		"link": "https://github.com/eeclaire/color-my-www",
+		"id": 1
+	},
+	{
 		"title": "Dan-ial",
 		"image": "src/img/projects/dan-ial_landing_page.png",
 		"description": "A website for when you need to express your disappointment in someone.",
 		"link": "https://github.com/eeclaire/dan-ial",
-		"id": 1
+		"id": 2
 	}
 ];
 
