ecae88fbc23ceeaf4db91926ed5357e22854edbe
Claire Durand <claire.durand@temple.edu>
2017-02-06 12:13:17 -0500

    added a build cmd to save index.html in proper location

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,7 +4,7 @@ const Resume =  React.createClass({
 	render() {
 		return (
 			<p>
-				<a href="./CV_Durand_Claire.pdf">Resume goes here.</a>
+				<a href="../CV_Durand_Claire.pdf">Resume goes here.</a>
 			</p>
 		);
 	}
