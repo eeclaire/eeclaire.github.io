18c1992c01ac1c4e67442dd54271eb15a6ddb9a1
Claire Durand <claire.durand@temple.edu>
2017-03-12 00:34:24 -0500

    added spacing, added artist queue projects

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -1,24 +1,38 @@
 const projectList = [
 	{
+		"title": "Artist Queue",
+		"image": "/src/img/projects/artist-queue.png",
+		"description": "Tracking artists I listen to so that I can be reminded they exist when I'm looking for music inspiration.",
+		"link": "https://github.com/eeclaire/artist-queue",
+		"id": 1
+	},
+	{
+		"title": "Hoverable Artist Pie",
+		"image": "/src/img/projects/artist-pie.png",
+		"description": "Made an interactive D3 visualization using the data accumulated from Artist Queue.",
+		"link": "https://bl.ocks.org/eeclaire/1a18f409b425f2bb6df5b03be0c482f0",
+		"id": 2
+	},
+	{
 		"title": "Color my www",
 		"image": "/src/img/projects/color-my-www_example.png",
 		"description": "Intelligent website color choices",
 		"link": "https://github.com/eeclaire/color-my-www",
-		"id": 1
+		"id": 3
 	},
 	{
 		"title": "Dan-ial",
 		"image": "/src/img/projects/dan-ial_landing_page.png",
 		"description": "A website for when you need to express your disappointment in someone.",
 		"link": "https://github.com/eeclaire/dan-ial",
-		"id": 2
+		"id": 4
 	},
 	{
 		"title": "Color visualization",
 		"image": "/src/img/projects/mountains_color.png",
 		"description": "Using Clarifai's color recognition model to explore the various colors that make up an image.",
 		"link": "https://github.com/eeclaire/color-visual",
-		"id": 3
+		"id": 5
 	}
 ];
 
